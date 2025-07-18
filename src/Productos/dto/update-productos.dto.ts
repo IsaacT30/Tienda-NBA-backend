ecea1f0fc5546d428162ae0e4c95b2fe4db182a9
IsaacT30 <isaaca.torres@ute.edu.ec>
2025-07-18 17:07:20 -0500

    Primer commit

--- src/Productos/dto/update-productos.dto.ts
+++ src/Productos/dto/update-productos.dto.ts
@@ -1,7 +1,31 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, IsNumber } from 'class-validator';
 
 export class UpdateProductoDto {
-  @IsString()
   @IsOptional()
-  name?: string;
+  @IsString()
+  nombre?: string;
+
+  @IsOptional()
+  @IsNumber()
+  precio?: number;
+
+  @IsOptional()
+  @IsString()
+  descripcion?: string;
+
+  @IsOptional()
+  @IsString()
+  imagen?: string;
+
+  @IsOptional()
+  @IsNumber()
+  stock?: number;
+
+  @IsOptional()
+  @IsString()
+  marca?: string;
+
+  @IsOptional()
+  @IsString()
+  categoria?: string;
 }
