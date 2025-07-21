ea7851c04758f375fab72f2a5f6883b176698c7c
IsaacT30 <isaaca.torres@ute.edu.ec>
2025-07-21 10:57:49 -0500

    fix: public path refactor

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,22 @@ import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filte
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+import * as fs from 'fs'; // opcional, solo para log/diagnóstico
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const publicPath = join(process.cwd(), 'public'); // <-- siempre apunta al root del proyecto
+  console.log('[Static] Serving from:', publicPath, 'exists?', fs.existsSync(publicPath));
+
+  app.useStaticAssets(publicPath, {
+    prefix: '/static/',  // o '/imagenes/' si prefieres
+  });
+
   app.enableCors();
   await app.listen(3050);
 }
 bootstrap();
-
